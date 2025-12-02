1ed89129561bf9a2c4c10dbc570db9282fcd3d59
shashank-amireddy <amireddy.shashank31@gmail.com>
2025-12-02 23:30:44 +0530

    Setup Neon and Drizzle

---
+++ src/models/user.model.js
@@ -0,0 +1,11 @@
+import { pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 256 }).notNull(),
+  email: varchar('email', { length: 256 }).notNull().unique(),
+  password: varchar('password', { length: 512 }).notNull(),
+  role: varchar('role', { length: 50 }).notNull().default('user'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
